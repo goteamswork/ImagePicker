418abbef777035778bcdcddbb09eec9082ff490c
shiv pogra <49437548+sspogra@users.noreply.github.com>
2025-01-14 15:17:08 +0530

    Update PhotoCaptureViewController.swift

--- Sources/PhotoCaptureViewController/PhotoCaptureViewController.swift
+++ Sources/PhotoCaptureViewController/PhotoCaptureViewController.swift
@@ -453,7 +453,8 @@ open class PhotoCaptureViewController: UIViewController, PhotoCollectionViewLayo
     }
 
     @objc func capturePhotoTapped(_ sender: UIButton) {
-        sender.isEnabled = (delegate?.photoCaptureViewControllerNumberOfAssets(self) ?? 0) >= self.maxImageCount
+        print("capturePhotoTapped:", (delegate?.photoCaptureViewControllerNumberOfAssets(self) ?? 0))
+        sender.isEnabled = false
         UIView.animate(withDuration: 0.1, animations: { self.previewView.alpha = 0.0 }, completion: { _ in
             UIView.animate(withDuration: 0.1, animations: { self.previewView.alpha = 1.0 })
         })
@@ -467,6 +468,8 @@ open class PhotoCaptureViewController: UIViewController, PhotoCollectionViewLayo
                 self.delegate?.photoCaptureViewController(self, didAddAsset: asset)
                 let insertedIndexPath: IndexPath
                 if let count = self.delegate?.photoCaptureViewControllerNumberOfAssets(self) {
+                    print("didAddAsset:", count)
+                    self.captureButton.isEnabled = count >= self.maxImageCount
                     insertedIndexPath = IndexPath(item: count - 1, section: 0)
                 } else {
                     insertedIndexPath = IndexPath(item: 0, section: 0)
