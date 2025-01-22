b4cefa3689e2ac7ddbd78b86526b610a2d2a2192
shiv pogra <49437548+sspogra@users.noreply.github.com>
2025-01-22 15:19:14 +0530

    Update PhotoCaptureViewController.swift

--- Sources/PhotoCaptureViewController/PhotoCaptureViewController.swift
+++ Sources/PhotoCaptureViewController/PhotoCaptureViewController.swift
@@ -430,9 +430,11 @@ open class PhotoCaptureViewController: UIViewController, PhotoCollectionViewLayo
 
            // let resolver = AssetResolver()
             var count = assets.count
+            print("assets:",assets)
             assets.forEach { image in
                 self.createAssetFromImage(image, completion: { (asset: Asset) in
                     var mutableAsset = asset
+                    print("asset:",asset)
                     mutableAsset.imageDataSourceType = .library
                     self.didAddAsset(mutableAsset)
                     
